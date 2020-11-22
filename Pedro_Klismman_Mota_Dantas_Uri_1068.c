6b3788b90b21c71e009561a38e60f6315bf10fc8
klismman <74274529+klismman@users.noreply.github.com>
2020-11-22 15:50:03 -0300

    Create Pedro_Klismman_Mota_Dantas_Uri_1068.c

---
+++ Pedro_Klismman_Mota_Dantas_Uri_1068.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+  char linha[1001];
+  int i;
+  int esquerda;
+  int direita;
+  while(scanf("%s", &linha[1001]) != EOF){
+       esquerda = 0;
+       direita = 0;
+       for(i = 0; linha[i] != '\0'; i++){
+             if(linha[i] == '(') esquerda++;
+             else if (linha[i] == ')'){
+                  direita++;
+                  if(esquerda > 0){
+                         esquerda--;
+                         direita--;
+                  }
+             }
+       }
+       if(esquerda == 0 && direita == 0) printf("correct\n");
+       else printf("incorrect\n");
+    }
+    return 0;
+}
