9ae0a0d9b111d7afff54d8fc04010072080b92c7
klismman <74274529+klismman@users.noreply.github.com>
2020-11-23 19:05:22 -0300

    Update Pedro_Klismman_Mota_Dantas_Uri_1069.c

--- Pedro_Klismman_Mota_Dantas_Uri_1069.c
+++ Pedro_Klismman_Mota_Dantas_Uri_1069.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-
+#include <string.h>
  
 #define true 1
 #define false 0 
 
-int main()
-{
-int i, j, quanti, diamante;
+int main(){
+
+int i, j, N, diamante;
 char areia[1010];
-quanti = 0;
-scanf("%d", &quanti);
+N = 0;
+scanf("%d", &N);
+
 while (true){
 
 scanf("%s", areia);
 diamante = 0;
 
-for (i = 0; i < strlen(areia); i++){
+for (i = 0; i < strlen (areia); i++){
             //NULL
 if (areia[i] == '\0')
-                break;
+            break;
+
 if (areia[i] == '<'){
 
 for (j = i; j< strlen(areia); j++){
@@ -27,7 +29,7 @@ if (areia[j] == '>'){
 
 diamante++;
 areia[j] = '0';
-                break;
+            break;
      }
     }
    }
